2439bfcc738ae306e51bd91b2fa553f9f6df5183
Shampoony <itnewsbgn@gmail.com>
2025-07-15 12:45:54 +0300

    dsddsdss

--- src/components/ratePage/v-rate.vue
+++ src/components/ratePage/v-rate.vue
@@ -15,109 +15,45 @@
             :initialSlide="0"
             :loop="false"
           >
-            <SwiperSlide class="v-rate__list-item rate">
-              <div class="rate__wave-top">
-                <div class="rate__image">
-                  <img src="/src/assets/images/dumbell.svg" alt="" />
-                </div>
-                <svg
-                  viewBox="0 0 100 60"
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="none"
-                >
-                  <path d="M0,20 Q25,0 50,20 T100,20 L100,0 L0,0 Z" fill="#00e5c0" />
-                  <path d="M0,35 Q25,-10 56,35 T100,35 L100,0 L0,0 Z" fill="#00e5c0" />
-                </svg>
-              </div>
-              <div class="rate__content">
-                <h3 class="rate__title">Базовый</h3>
-                <h2 class="rate__price">20$</h2>
+            <SwiperSlide
+              v-for="rate in rates"
+              :key="rate.id"
+              class="v-rate__list-item"
+              @click="selectRate(rate.id)"
+            >
+              <div class="rate" :class="{ selected: selectedRate === rate.id }">
+                <div class="rate__inner">
+                  <div class="rate__wave-top">
+                    <div class="rate__image">
+                      <img :src="rate.icon" alt="" />
+                    </div>
+                    <svg
+                      viewBox="0 0 100 60"
+                      xmlns="http://www.w3.org/2000/svg"
+                      preserveAspectRatio="none"
+                    >
+                      <path d="M0,20 Q25,0 50,20 T100,20 L100,0 L0,0 Z" fill="#00e5c0" />
+                      <path d="M0,35 Q25,-10 56,35 T100,35 L100,0 L0,0 Z" fill="#00e5c0" />
+                    </svg>
+                  </div>
+                  <div class="rate__content">
+                    <h3 class="rate__title">{{ rate.title }}</h3>
+                    <h2 class="rate__price">{{ rate.price }}</h2>
 
-                <ul class="rate__list">
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ 1 месяц доступ</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ План тренировок</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ План питания</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ Анализ результатов</p>
-                  </li>
-                </ul>
-                <button class="rate__button">Купить</button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide class="v-rate__list-item rate">
-              <div class="rate__wave-top">
-                <div class="rate__image">
-                  <img src="/src/assets/images/dumbell.svg" alt="" />
+                    <ul class="rate__list">
+                      <li
+                        v-for="(feature, index) in rate.features"
+                        :key="index"
+                        class="rate__list-item"
+                      >
+                        <p class="rate__subtitle">✓ {{ feature }}</p>
+                      </li>
+                    </ul>
+                    <button class="rate__button">
+                      {{ selectedRate === rate.id ? 'Выбран' : 'Купить' }}
+                    </button>
+                  </div>
                 </div>
-                <svg
-                  viewBox="0 0 100 60"
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="none"
-                >
-                  <path d="M0,20 Q25,0 50,20 T100,20 L100,0 L0,0 Z" fill="#00e5c0" />
-                  <path d="M0,35 Q25,-10 56,35 T100,35 L100,0 L0,0 Z" fill="#00e5c0" />
-                </svg>
-              </div>
-              <div class="rate__content">
-                <h3 class="rate__title">Базовый</h3>
-                <h2 class="rate__price">20$</h2>
-
-                <ul class="rate__list">
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ 1 месяц доступ</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ План тренировок</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ План питания</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ Анализ результатов</p>
-                  </li>
-                </ul>
-                <button class="rate__button">Купить</button>
-              </div>
-            </SwiperSlide>
-            <SwiperSlide class="v-rate__list-item rate">
-              <div class="rate__wave-top">
-                <div class="rate__image">
-                  <img src="/src/assets/images/dumbell.svg" alt="" />
-                </div>
-                <svg
-                  viewBox="0 0 100 60"
-                  xmlns="http://www.w3.org/2000/svg"
-                  preserveAspectRatio="none"
-                >
-                  <path d="M0,20 Q25,0 50,20 T100,20 L100,0 L0,0 Z" fill="#00e5c0" />
-                  <path d="M0,35 Q25,-10 56,35 T100,35 L100,0 L0,0 Z" fill="#00e5c0" />
-                </svg>
-              </div>
-              <div class="rate__content">
-                <h3 class="rate__title">Базовый</h3>
-                <h2 class="rate__price">20$</h2>
-
-                <ul class="rate__list">
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ 1 месяц доступ</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ План тренировок</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ План питания</p>
-                  </li>
-                  <li class="rate__list-item">
-                    <p class="rate__subtitle">✓ Анализ результатов</p>
-                  </li>
-                </ul>
-                <button class="rate__button">Купить</button>
               </div>
             </SwiperSlide>
           </swiper>
@@ -129,8 +65,38 @@
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import 'swiper/css'
 import { Swiper, SwiperSlide } from 'swiper/vue'
-
 import vFooter from '@/components/generalComponents/v-footer.vue'
+
+const selectedRate = ref(null)
+
+const rates = ref([
+  {
+    id: 1,
+    title: 'Базовый',
+    price: '20$',
+    icon: '/src/assets/images/dumbell.svg',
+    features: ['1 месяц доступ', 'План тренировок', 'План питания', 'Анализ результатов'],
+  },
+  {
+    id: 2,
+    title: 'Стандарт',
+    price: '35$',
+    icon: '/src/assets/images/dumbell.svg',
+    features: ['1 месяц доступ', 'План тренировок', 'План питания', 'Анализ результатов'],
+  },
+  {
+    id: 3,
+    title: 'Премиум',
+    price: '50$',
+    icon: '/src/assets/images/dumbell.svg',
+    features: ['1 месяц доступ', 'План тренировок', 'План питания', 'Анализ результатов'],
+  },
+])
+
+const selectRate = (rateId) => {
+  selectedRate.value = rateId
+}
 </script>
